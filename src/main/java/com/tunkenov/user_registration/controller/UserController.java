9175633f8814d40bf2a8bb9373870e9296c1baa9
TunkenovNikolay <nik0sh@bk.ru>
2021-04-19 18:46:13 +0700

    Исправление ошибок и добавления Swagger UI 2.

--- src/main/java/com/tunkenov/user_registration/controller/UserController.java
+++ src/main/java/com/tunkenov/user_registration/controller/UserController.java
@@ -2,6 +2,8 @@ package com.tunkenov.user_registration.controller;
 
 import com.tunkenov.user_registration.dto.UserDTO;
 import com.tunkenov.user_registration.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,31 +17,36 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation("Загрузка всех пользователей.")
     @GetMapping
     public List<UserDTO> findAllUsers() {
         return userService.findAll();
     }
 
+    @ApiOperation("Загрузка пользователя по id.")
     @GetMapping("/{id}")
-    public UserDTO findUser(@PathVariable("id") Long id) {
+    public UserDTO findUser(@ApiParam("ID пользователя.") @PathVariable("id") Long id) {
         return userService.getOne(id);
     }
 
+    @ApiOperation("Регистрация нового пользователя.")
     @PutMapping
     @ResponseBody
-    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+    public UserDTO saveUser(@ApiParam("Имя и Фамилия пользователя.") @RequestBody UserDTO userDTO) {
         return userService.saveUser(userDTO);
     }
 
+    @ApiOperation("Редактирование информации пользователя по id.")
     @PostMapping("/{id}")
     @ResponseBody
-    public UserDTO updateUser(@PathVariable("id") Long id, @RequestBody UserDTO userDTO) {
+    public UserDTO updateUser(@ApiParam("ID пользователя.") @PathVariable("id") Long id,
+                              @ApiParam("Новые данные пользователя.") @RequestBody UserDTO userDTO) {
         return userService.updateUser(id, userDTO);
     }
 
+    @ApiOperation("Удаление пользователя по id.")
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable("id") Long id){
+    public void deleteUser(@ApiParam("ID пользователя.") @PathVariable("id") Long id) {
         userService.deleteUser(id);
     }
-
 }
--- src/main/java/com/tunkenov/user_registration/model/repository/UserRepository.java
+++ src/main/java/com/tunkenov/user_registration/model/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.tunkenov.user_registration.model.repository;
 
 import com.tunkenov.user_registration.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
